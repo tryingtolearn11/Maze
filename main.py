634dd2993e01edb108ff5fa28d4f0942345c7399
DAMIEN SINGH <dsingh005@citymail.cuny.edu>
2020-07-23 15:38:16 -0400

    '

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 pygame.init()
 screenWidth = 604
 screenHeight = 604
@@ -7,7 +7,7 @@ screenHeight = 604
 # Colors
 Red = (255, 0, 0)
 Blue = (0, 0, 255)
-Green = (0, 255, 0)
+Green = (0, 200, 0)
 White = (255, 255, 255)
 
 
@@ -15,14 +15,15 @@ class Cell:
     def __init__(self, i, j):
         self.i = i  # Row
         self.j = j  # Column
+        self.visited = False
         # Checks to see if wall(s) of the cell exists
         # Order:   Top,  Right, Bottom, Left
         self.wall = [True, True, True, True]
+        self.neighbors = checkN()
 
     def draw(self, surface):
         x = self.i * length
         y = self.j * length
-        # pygame.draw.rect(displayWindow, White, (x, y, length, length), 1)
         # Top Line                             Start Pos,  End Pos
         if self.wall[0]:
             pygame.draw.line(displayWindow, White, (x, y), (x + length, y), 1)
@@ -35,10 +36,38 @@ class Cell:
         # Left
         if self.wall[3]:
             pygame.draw.line(displayWindow, White, (x, y + length), (x, y), 1)
+        if self.visited:
+            pygame.draw.rect(displayWindow, Green, (x, y, length, length))
+
+    def checkN():
+        neighbors = []
+        # Indexing
+        top = grid[index(i, j - 1)]
+        right = grid[index(i + 1, j)]
+        bottom = grid[index(i, j + 1)]
+        left = grid[index(i - 1, j)]
+
+        # List of Unvisited Cells, cells must be defined and Unvisited
+        if top and top.visited is False:
+            neighbors.append(top)
+        if right and right.visited is False:
+            neighbors.append(right)
+        if bottom and bottom.visited is False:
+            neighbors.append(bottom)
+        if left and left.visited is False:
+            neighbors.append(left)
+
+        if len(neighbors) > 0:
+            p = random.choice(neighbors)
+            return neighbors[p]
+        else:
+            return -1
+
+
 
 
 # Rows and Columns, length will be our width of each cell
-global rows, cols, length
+global rows, cols, length, current
 length = 40
 rows = screenWidth // length
 cols = screenHeight // length
@@ -48,11 +77,22 @@ for i in range(rows):
     for j in range(cols):
         cell = Cell(i, j)
         grid.append(cell)
+current = grid[0]
+
+
+def index(i, j):
+    # Test edge
+    if i < 0 or i > rows - 1 or j < 0 or j > cols - 1:
+        return -1
+    else:
+        return i + j * cols
 
 
 def display(surface):
     for i in range(len(grid)):
         grid[i].draw(surface)
+    # Mark first cell as visited
+    current.visited = True
 
 
 displayWindow = pygame.display.set_mode((screenWidth, screenHeight))
