1a6eff7579b0529fa266a7256e4c1c6e07ad3b30
DAMIEN SINGH <dsingh005@citymail.cuny.edu>
2020-07-23 14:41:30 -0400

    'added walls to cells'

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame
 
 pygame.init()
-screenWidth = 600
-screenHeight = 600
+screenWidth = 604
+screenHeight = 604
 
 # Colors
 Red = (255, 0, 0)
@@ -13,16 +13,31 @@ White = (255, 255, 255)
 
 class Cell:
     def __init__(self, i, j):
-        self.i = i
-        self.j = j
+        self.i = i  # Row
+        self.j = j  # Column
+        # Checks to see if wall(s) of the cell exists
+        # Order:   Top,  Right, Bottom, Left
+        self.wall = [True, True, True, True]
 
     def draw(self, surface):
         x = self.i * length
         y = self.j * length
-        pygame.draw.rect(displayWindow, White, (x, y, length, length), 2)
+        # pygame.draw.rect(displayWindow, White, (x, y, length, length), 1)
+        # Top Line                             Start Pos,  End Pos
+        if self.wall[0]:
+            pygame.draw.line(displayWindow, White, (x, y), (x + length, y), 1)
+        # Right Line
+        if self.wall[1]:
+            pygame.draw.line(displayWindow, White, (x + length, y), (x + length, y + length), 1)
+        # Bottom
+        if self.wall[2]:
+            pygame.draw.line(displayWindow, White, (x + length, y + length), (x, y + length), 1)
+        # Left
+        if self.wall[3]:
+            pygame.draw.line(displayWindow, White, (x, y + length), (x, y), 1)
 
 
-# Rows and Colums
+# Rows and Columns, length will be our width of each cell
 global rows, cols, length
 length = 40
 rows = screenWidth // length
@@ -41,7 +56,7 @@ def display(surface):
 
 
 displayWindow = pygame.display.set_mode((screenWidth, screenHeight))
-pygame.display.set_caption("Maze")
+pygame.display.set_caption("Maze Generator")
 
 
 def main():
