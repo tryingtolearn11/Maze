8b881429a001a6e28d74748f5350f5b60b262458
DAMIEN SINGH <dsingh005@citymail.cuny.edu>
2020-07-23 00:03:13 -0400

    ...

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 import pygame
 
-
 pygame.init()
 screenWidth = 400
 screenHeight = 400
+
+
+class Cell:
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+
+    def draw(self, surface):
+        x = self.i * length
+        y = self.j * length
+        self.Rect(x, y, length, length)
+        self.pygame.draw.rect(displayWindow, (255, 255, 0), s)
+
+
+# Rows and Colums
+global rows, cols, length
+length = 40
+rows = screenWidth // length
+cols = screenHeight // length
+# store cells
+grid = []
+for i in range(rows):
+    for j in range(cols):
+        cell = Cell(i, j)
+        grid.append(cell)
+
+
+def display():
+    for x in range(len(grid)):
+        grid[i].draw(displayWindow)
+
+
 displayWindow = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("Maze")
 
@@ -15,6 +46,7 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
+    display()
     pygame.display.update()
 
 
