9f19073b4bc3d3867b5e1262e35d5892a4631493
DAMIEN SINGH <dsingh005@citymail.cuny.edu>
2020-08-02 17:46:08 -0400

    button function, ui stuff

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 import random
-screenWidth = 800
-screenHeight = 800
+pygame.init()
+screenWidth = 1000
+screenHeight = 1000
 FPS = 50
 
 # Colors
@@ -10,6 +11,8 @@ Blue = (0, 100, 200, 50)
 Green = (0, 200, 100, 50)
 White = (255, 255, 255)
 Black = (0, 0, 0)
+bgcolor = (5, 55, 75)
+BASICFONTSIZE = 18
 
 
 class Cell:
@@ -17,14 +20,14 @@ class Cell:
         self.i = i  # Row
         self.j = j  # Column
         self.visited = False
+        # Checks to see if wall(s) of the cell exists
         # Order:   Top,  Right, Bottom, Left
         self.wall = [True, True, True, True]
         self.backtrackPathColor = False
 
     def draw(self, surface):
-        x = self.i * length
-        y = self.j * length
-
+        x = self.i * length + xmargin
+        y = self.j * length + ymargin
         if self.backtrackPathColor:
             pygame.draw.rect(displayWindow, Black, (x, y, length, length))
         elif self.visited:
@@ -78,8 +81,8 @@ class Cell:
             return self.neighbors[p]
 
     def marker(self):
-        x = self.i * length
-        y = self.j * length
+        x = self.i * length + xmargin
+        y = self.j * length + ymargin
         pygame.draw.rect(displayWindow, Blue, (x, y, length, length))
 
 
@@ -92,6 +95,9 @@ cols = borderHeight // length
 
 # Add cells to stack
 stack = []
+
+xmargin = int((screenWidth - (length * rows + (cols - 1))) / 2)
+ymargin = int((screenHeight - (length * cols + (rows - 1))) / 2)
 
 # store cells in the grid
 grid = []
@@ -109,12 +115,25 @@ x = len(grid)
 y = len(grid[i])
 
 
+def leftTopofTile(a, b):
+    left = xmargin + (a * length) + (a - 1)
+    top = ymargin + (b * length) + (b - 1)
+    return (left, top)
+
+
 def display(surface):
     global current
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             grid[i][j].draw(surface)
 
+    # Drawing the border
+    left, top = leftTopofTile(0, 0)
+    width = cols * length
+    height = rows * length
+    pygame.draw.rect(displayWindow, Blue, (left - 3, top - 3, width + 9, height + 9), 5)
+    # Draw Buttons
+    drawButtons()
     current.visited = True
     current.marker()
     next = current.countNeighbors(grid)
@@ -147,16 +166,27 @@ def deleteWall(a, b):
         b.wall[0] = False
 
 
-displayWindow = pygame.display.set_mode((screenWidth, screenHeight))
-pygame.display.set_caption("Maze Generator")
+def makeTextBox(text, color, bgcolor, top, left):
+    textSurface = BASICFONT.render(text, True, color, bgcolor)
+    textRect = textSurface.get_rect()
+    textRect.topleft = (top, left)
+    return (textSurface, textRect)
+
+
+def drawButtons():
+    displayWindow.blit(RESET_SURF, RESET_RECT)
 
 
 def main():
-    global FPS
-    pygame.init()
+    global displayWindow, FPS, BASICFONT, RESET_SURF, RESET_RECT
     FPSclock = pygame.time.Clock()
+    displayWindow = pygame.display.set_mode((screenWidth, screenHeight))
+    pygame.display.set_caption("Maze Generator")
+    BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
+    RESET_SURF, RESET_RECT = makeTextBox('Reset', Black, White, screenWidth - 120, screenHeight - 90)
     start = False
     running = True
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -164,10 +194,12 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     start = True
+
         if(start):
             display(displayWindow)
             pygame.display.update()
             FPSclock.tick(FPS)
+        displayWindow.fill(bgcolor)
 
 
 if __name__ == '__main__':
