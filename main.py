142ba151255492ec0fd9a121d1ed97f419aabfdd
DAMIEN SINGH <dsingh005@citymail.cuny.edu>
2020-08-03 15:11:53 -0400

    ...

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import pygame
 import random
-pygame.init()
 screenWidth = 1000
 screenHeight = 1000
-FPS = 50
+FPS = 40
 
+
+# TODO: IMPLEMENT A* Pathfinding Algorithm
 # Colors
-Red = (255, 0, 0)
+Red = (200, 5, 50, 50)
 Blue = (0, 100, 200, 50)
 Green = (0, 200, 100, 50)
 White = (255, 255, 255)
 Black = (0, 0, 0)
 bgcolor = (5, 55, 75)
-BASICFONTSIZE = 18
+BASICFONTSIZE = 20
 
 
 class Cell:
     def __init__(self, i, j):
-        self.i = i  # Row
-        self.j = j  # Column
+        self.i = i
+        self.j = j
         self.visited = False
-        # Checks to see if wall(s) of the cell exists
         # Order:   Top,  Right, Bottom, Left
         self.wall = [True, True, True, True]
         self.backtrackPathColor = False
@@ -45,7 +45,6 @@ class Cell:
         if self.wall[3]:
             pygame.draw.line(displayWindow, White, (x, y + length), (x, y), 1)
 
-    # Function adds the Unvisited to the 2D array and returns the next cell
     def countNeighbors(self, grid):
         global x, y
         self.neighbors = []
@@ -66,7 +65,6 @@ class Cell:
         else:
             left = None
 
-        # If the neighbor cell is unvisited add to array
         if top and not top.visited:
             self.neighbors.append(top)
         if right and not right.visited:
@@ -75,7 +73,7 @@ class Cell:
             self.neighbors.append(bottom)
         if left and not left.visited:
             self.neighbors.append(left)
-        # pick random unvisted cell as our next
+        # pick random unvisted cell
         if len(self.neighbors):
             p = random.randrange(len(self.neighbors))
             return self.neighbors[p]
@@ -83,15 +81,26 @@ class Cell:
     def marker(self):
         x = self.i * length + xmargin
         y = self.j * length + ymargin
-        pygame.draw.rect(displayWindow, Blue, (x, y, length, length))
+        if self.i == 0 and self.j == 0:
+            pygame.draw.rect(displayWindow, Black, (x, y, length, length))
+        else:
+            pygame.draw.rect(displayWindow, Blue, (x, y, length, length))
+
+    def resetCells(self):
+        self.visited = False
+        self.wall = [True, True, True, True]
+        self.backtrackPathColor = False
+        if len(self.neighbors):
+            self.neighbors.clear()
 
 
-# Rows and Columns, length will be our width of each cell
+# Rows and Columns
 length = 40
 borderWidth = 800
 borderHeight = 800
 rows = borderWidth // length
 cols = borderHeight // length
+print(rows, cols)
 
 # Add cells to stack
 stack = []
@@ -99,7 +108,7 @@ stack = []
 xmargin = int((screenWidth - (length * rows + (cols - 1))) / 2)
 ymargin = int((screenHeight - (length * cols + (rows - 1))) / 2)
 
-# store cells in the grid
+# store cells
 grid = []
 for i in range(rows):
     column = []
@@ -108,13 +117,13 @@ for i in range(rows):
         column.append(cell)
     grid.append(column)
 
-
 current = grid[0][0]
 
 x = len(grid)
 y = len(grid[i])
 
 
+# Tile -> Pixel Coords
 def leftTopofTile(a, b):
     left = xmargin + (a * length) + (a - 1)
     top = ymargin + (b * length) + (b - 1)
@@ -131,9 +140,15 @@ def display(surface):
     left, top = leftTopofTile(0, 0)
     width = cols * length
     height = rows * length
-    pygame.draw.rect(displayWindow, Blue, (left - 3, top - 3, width + 9, height + 9), 5)
+    pygame.draw.rect(displayWindow, Blue, (left - 3, top - 3, width + 10, height + 10), 5)
     # Draw Buttons
     drawButtons()
+    if START:
+        update()
+
+
+def update():
+    global current
     current.visited = True
     current.marker()
     next = current.countNeighbors(grid)
@@ -145,6 +160,14 @@ def display(surface):
     elif len(stack):
         current.backtrackPathColor = True
         current = stack.pop()
+
+
+def reset(surface):
+    global current
+    for i in range(x):
+        for j in range(y):
+            grid[i][j].resetCells()
+    current = grid[0][0]
 
 
 def deleteWall(a, b):
@@ -175,16 +198,30 @@ def makeTextBox(text, color, bgcolor, top, left):
 
 def drawButtons():
     displayWindow.blit(RESET_SURF, RESET_RECT)
+    displayWindow.blit(START_SURF, START_RECT)
+
+
+def getMouseClick(surface, xpos, ypos):
+    for i in range(x):
+        for j in range(y):
+            left, top = leftTopofTile(xpos, ypos)
+            tileRect = pygame.Rect(left, top, length, length)
+            if tileRect.collidepoint(xpos, ypos):
+                return(xpos, ypos)
+    return (None, None)
 
 
 def main():
-    global displayWindow, FPS, BASICFONT, RESET_SURF, RESET_RECT
+    global displayWindow, FPS, BASICFONT, RESET_SURF, RESET_RECT, START_SURF, START_RECT, START
+    pygame.init()
     FPSclock = pygame.time.Clock()
     displayWindow = pygame.display.set_mode((screenWidth, screenHeight))
     pygame.display.set_caption("Maze Generator")
-    BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
-    RESET_SURF, RESET_RECT = makeTextBox('Reset', Black, White, screenWidth - 120, screenHeight - 90)
-    start = False
+    BASICFONT = pygame.font.SysFont('arial', BASICFONTSIZE)
+    RESET_SURF, RESET_RECT = makeTextBox('Reset', White, Black, screenWidth - 150, screenHeight - 90)
+    START_SURF, START_RECT = makeTextBox('Start', White, Black, screenWidth - 910, screenHeight - 90)
+
+    START = False
     running = True
 
     while running:
@@ -193,12 +230,19 @@ def main():
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    start = True
+                    START = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                x_spot, y_spot = getMouseClick(displayWindow, event.pos[0], event.pos[1])
+                if (x_spot, y_spot) == (None, None):
+                    if START_RECT.collidepoint(event.pos):
+                        START = True
+                    if RESET_RECT.collidepoint(event.pos):
+                        pygame.time.wait(500)
+                        reset(displayWindow)
 
-        if(start):
-            display(displayWindow)
-            pygame.display.update()
-            FPSclock.tick(FPS)
+        display(displayWindow)
+        pygame.display.update()
+        FPSclock.tick(FPS)
         displayWindow.fill(bgcolor)
 
 
